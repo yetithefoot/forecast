4cd0a826544b3cbe600265dc0a0e0eb5bc16a653
DESKTOP-3URLA53\21pink-React <pavlo.andriiesh@gmail.com>
2017-04-22 16:48:33 +0300

    getting location by IP using ipinfo.io

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,8 +1,13 @@
 const Forecast = require('forecast')
 const config = require('../utils/config')
+const request = require('superagent')
+
+const Bali = {
+  latitude: -8.409518,
+  longitude: 115.188919
+}
 
 exports.forecast = (req, res) => {
-  const {latitude, longitude} = req.query
   const forecast = new Forecast({
     service: 'darksky',
     key: config.key,
@@ -10,10 +15,37 @@ exports.forecast = (req, res) => {
     cache: true
   })
 
-  forecast.get([latitude, longitude], (err, weather) => {
-    if (err) return res.status(404).send(err)
-    res.send(weather)
-  })
+  let {latitude, longitude} = req.query
+
+  const getForecast = (latitude, longitude) => {
+    forecast.get([latitude, longitude], (err, weather) => {
+      if (err) return res.status(404).send(err)
+      res.send(weather)
+    })
+  }
+
+  if (!latitude || !longitude) {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    if (!ip || ip === '::1') {
+      getForecast(Bali.latitude, Bali.longitude)
+    } else {
+      request
+        .get('http://ipinfo.io/' + ip)
+        .set('Accept', 'application/json')
+        .end((err, response) => {
+          if (err || !response.ok) {
+            getForecast(Bali.latitude, Bali.longitude)
+          } else {
+            latitude = response.body.loc.split(',')[0]
+            longitude = response.body.loc.split(',')[1]
+
+            getForecast(latitude, longitude)
+          }
+        })
+    }
+  } else {
+    getForecast(latitude, longitude)
+  }
 }
 
 exports.client = (req, res) => {
